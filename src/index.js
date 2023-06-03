18840905cdae938af24ff7a468724366f4654ca9
marwan211 <samroutc@gmail.com>
2023-06-03 20:10:54 +0300

    update7

--- src/index.js
+++ src/index.js
@@ -3,50 +3,27 @@ const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
 
-// Initialize the Express app
 const app = express();
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+app.use(cors({
+  origin: 'http://127.0.0.1:5173'
+}));
 const server = http.createServer(app);
-
-// Enable CORS for the entire server
-server.on('request', cors());
-
 const port = process.env.PORT || 5000;
-
-// Start the server
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
-// Initialize Socket.IO
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  }
+});
 let brightness = 0;
-
-// Handle incoming WebSocket connections
 io.on('connection', (socket) => {
   console.log('New WebSocket connection');
-
-  // Get the current LED brightness and send it to the client
-  socket.emit('brightness', brightness); // Assuming initial brightness is 0
-
-  // Listen for changes in brightness from the client
+  socket.emit('brightness', brightness);
   socket.on('brightness', (newBrightness) => {
-    // Update the brightness value
     brightness = newBrightness;
-
-    // Log the updated brightness value
     console.log('Brightness updated:', brightness);
-
-    // Emit the updated brightness value to all connected clients
     io.emit('brightness', brightness);
-
-    // You can add your code here to control the Arduino LED based on the brightness value
-    // For example, you can use the 'johnny-five' library to communicate with the Arduino over a serial port
-    // to adjust the LED brightness accordingly
   });
 });
