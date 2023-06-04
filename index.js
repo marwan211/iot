f209e4578e44fee7e5f1a86f03ecd20b12ee459a
marwan211 <samroutc@gmail.com>
2023-06-04 13:44:58 +0300

    update10

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+const app = express();
+app.use(cors({
+  origin: '*'
+}));
+const server = http.createServer(app);
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+const io = new Server(server, {
+  cors: {
+   origin: "*",
+   methods: ["GET", "POST"],
+   transports: ["websocket", "polling"],
+   credentials: true,
+  },
+   allowEIO3: true,
+  });
+
+let brightness = 0;
+io.on('connection', (socket) => {
+  console.log('New WebSocket connection');
+  socket.emit('brightness', brightness);
+  socket.on('brightness', (newBrightness) => {
+    brightness = newBrightness;
+    console.log('Brightness updated:', brightness);
+    io.emit('brightness', brightness);
+  });
+});
